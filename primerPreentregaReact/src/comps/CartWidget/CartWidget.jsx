8f95c65a58f2c03930283b01228a5b291ac50d12
Jonathan Abracaite <61628671+jonathanAbracaite@users.noreply.github.com>
2023-12-17 16:28:11 -0300

    edit

--- primerPreentregaReact/src/comps/CartWidget/CartWidget.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { RiShoppingCartLine } from "react-icons/ri";
-const CartWidget = () => {
-  return (
-    <RiShoppingCartLine />
-  )
-}
-
-export default CartWidget
