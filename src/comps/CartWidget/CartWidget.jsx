bf51bd58b0e95e407d0bae212578951ee66c6af7
Jonathan Abracaite <61628671+jonathanAbracaite@users.noreply.github.com>
2023-12-17 16:39:45 -0300

    del

--- src/comps/CartWidget/CartWidget.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { RiShoppingCartLine } from "node_modules/react-icons/ri";
-const CartWidget = () => {
-  return (
-    <RiShoppingCartLine />
-  )
-}
-
-export default CartWidget
